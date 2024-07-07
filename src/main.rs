6286c18722ce3e6739446197ec0db75b558d2b99
mitani <nico.programming1011@gmail.com>
2024-07-08 00:22:32 +0900

    修正

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ mod sample1;
 fn main() {
     sample1::print();
     sample1::value_print();
+    sample1::if_test();
 }
--- src/sample1/mod.rs
+++ src/sample1/mod.rs
@@ -3,8 +3,15 @@ pub fn print() {
 }
 
 pub fn value_print() {
-    let mut x = 5;
+    let mut x: i32 = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+}
+
+pub fn if_test() {
+  let number = 1;
+    if number != 0 {
+        println!("number was something other than zero");
+    }
 }
